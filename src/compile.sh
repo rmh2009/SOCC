9164a052030bc66db031b772e75222f17998162c
Hong <19938173+rmh2009@users.noreply.github.com>
2020-04-15 16:32:26 -0400

    update the helper script.

--- src/compile.sh
+++ src/compile.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+MODE=$1
+
+if [ -z $MODE ]; then
+  echo "Unspecified mode, must be either 'compile' or 'run'"
+  exit 1
+fi
+
+if [ $MODE == "compile" ]; then
 # Compile the compiler, then compile the test.cc file into assembly.s.
 ocamlopt -o cc_ocaml lexer.ml parser.ml codegen.ml
 ./cc_ocaml
+fi
 
+if [ $MODE == "run" ]; then
 # Compile the generated assembly into executable and run.
-# /usr/local/Cellar/gcc/9.3.0/bin/gcc-9 -m32 assembly.s -o out
-# ./out
-
+# Use the brew installed gcc since the default gcc in macos no longer supports 32bit.
+/usr/local/Cellar/gcc/9.3.0/bin/gcc-9 -m32 assembly.s -o out
+./out
+echo "output is $?!"
+fi
